3c0267378d7d246cd7bb140f9f8abdeffa2b6d4f
rm-richard <arvarichi@gmail.com>
2019-09-05 13:38:04 +0200

    farm format and tier base

--- commands/farm.js
+++ commands/farm.js
@@ -1,33 +1,61 @@
 const Enmap = require('enmap');
+const Discord = require('discord.js');
 
-const FARM_DELAY_MS = 20 * 1000;
+const FARM_DELAY_MS = 20 * 60 * 1000;
 
 exports.run = (client, message, args) => {
-  if (!client.farmDb) client.farmDb = new Enmap({name: 'farmDb'});
+  if (!client.farmDb) client.farmDb = new Enmap({name: 'farmDb', ensureProps: true});
 
   const key = message.author.id;
 
   client.farmDb.ensure(key, {
     user: message.author.id,
     copper: 0,
-    lastFarmed: 0
+    lastFarmed: 0,
+    farmTier: 0
   });
 
-  const currentTime = new Date().getTime();
-  const farmAvailable = client.farmDb.get(key, 'lastFarmed') + FARM_DELAY_MS;
+  const farmTierIdx = client.farmDb.get(key, 'farmTier');
+  const farmTier = client.config.farmTiers[farmTierIdx];
+  const totalCopper = client.farmDb.get(key, 'copper');
 
-  if (farmAvailable < currentTime) {
-    const farmedCopper = generateRandomInteger(45, 300);
-    client.farmDb.math(key, "+", farmedCopper, 'copper');
-    client.farmDb.set(key, currentTime, 'lastFarmed');
-    const totalCopper = client.farmDb.get(key, 'copper');
-
-    message.reply(`you just farmed ${formatWallet(farmedCopper)}!`);
-    message.reply(`you now have ${formatWallet(totalCopper)}!`);
+  if (args[0] === 'invest') {
+    invest(key, client, message);
   } else {
+    // standard farm + status flow
+    const currentTime = new Date().getTime();
+    const farmAvailable = client.farmDb.get(key, 'lastFarmed') + FARM_DELAY_MS;
     const diffSec = (farmAvailable - currentTime) / 1000;
-    message.reply(`you can farm in ${Math.floor(diffSec/60).toFixed()} minutes ${(diffSec % 60).toFixed()} seconds!`)
+
+    const reply = new Discord.RichEmbed();
+
+    if (farmAvailable < currentTime) {
+      const farmedCopper = generateRandomInteger(45, 300);
+      client.farmDb.math(key, "+", farmedCopper, 'copper');
+      client.farmDb.set(key, currentTime, 'lastFarmed');
+
+      reply.setColor('#0cf246')
+        .setTitle('Farming successful!')
+        .setDescription(`<@${message.author.id}>, you just gained ${formatWallet(farmedCopper)}! Check back in 20 minutes.`);
+    }
+    else {
+      reply.setColor('#a30a0a')
+        .setTitle('Farming failed!')
+        .setDescription(`<@${message.author.id}>, you can next farm in ${Math.floor(diffSec/60).toFixed()} minutes ${(diffSec % 60).toFixed()} seconds!`);
+    }
+
+    reply.addBlankField()
+      .addField('Total gold', formatWallet(client.farmDb.get(key, 'copper')), true)
+      .addField('Farm tier', farmTier.name, true)
+      .addField('Next tier cost', formatWallet(farmTier.investCost), true)
+      .addBlankField()
+      .setFooter("To upgrade your farm tier, type '!farm invest'");
+    message.channel.send(reply);
   }
+}
+
+function invest(key, client, message) {
+  message.reply('investing is not implemented yet!');
 }
 
 function generateRandomInteger(min, max) {
