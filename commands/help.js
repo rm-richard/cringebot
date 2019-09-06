963e1cc38f1bf9ef121c8c93b62369aae1d0b062
rm-richard <arvarichi@gmail.com>
2019-09-06 19:33:48 +0200

    add help, remove mirror commands

---
+++ commands/help.js
@@ -0,0 +1,16 @@
+const helpMessage = `\`\`\`
+Supported commands:
+  !farm          - farm for gold every 20 minutes
+  !farm invest   - upgrade your farm if you have enough gold
+
+  !duel @mention - challenge another user to a duel
+  !duel accept   - accept a duel challenge
+
+  !played        - display your total time spent in WoW
+
+  !help          - display this message
+\`\`\``;
+
+exports.run = (client, message, args) => {
+  message.channel.send(helpMessage);
+}
