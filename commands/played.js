c2e97a88f301e662d00b3afe0c937688c23afab0
rm-richard <arvarichi@gmail.com>
2019-09-06 19:22:45 +0200

    refactor around playtime

--- commands/played.js
+++ commands/played.js
@@ -6,22 +6,21 @@ const playtime = require('../lib/playtime.js');
 exports.run = (client, message, args) => {
   const id = message.author.id;
 
-  client.playDb.ensure(id, {playtime: 0});
+  client.playDb.ensure(id, {totalTime: 0});
 
   const currentTime = new Date().getTime();
   const game = message.author.presence.game;
-  const isOnline = game && game.name.toLowerCase().startsWith("world of warcraft");
-  const onlineFor = game ? currentTime - game.timestamps.start.getTime() : null;
 
   var totalTime;
   var reply = '';
 
-  if (isOnline) {
+  if (playtime.isPlayingWow(game)) {
+    const onlineFor = currentTime - game.timestamps.start.getTime();
     reply += `you are currently playing WoW for **${format.toDisplayedTime(onlineFor)}**. `;
     totalTime = playtime.update(client, id, game);
   } else {
     reply += 'you are not playing WoW right now. ';
-    totalTime = client.playDb.get(id, 'playtime');
+    totalTime = client.playDb.get(id, 'totalTime');
   }
 
   reply += `\nYour total ingame time is **${format.toDisplayedTime(totalTime)}**.`;
