8e68dd9d7351eda26a1a3cbfb2d966d080b55786
rm-richard <arvarichi@gmail.com>
2019-09-09 11:05:16 +0200

    fatigue level, shortened wallet format

--- commands/farm.js
+++ commands/farm.js
@@ -2,8 +2,6 @@ const Enmap = require('enmap');
 const Discord = require('discord.js');
 const format = require('../lib/formatters.js');
 
-const FARM_DELAY_MS = 20 * 60 * 1000;
-
 exports.run = (client, message, args) => {
   const key = message.author.id;
 
@@ -11,7 +9,8 @@ exports.run = (client, message, args) => {
     user: message.author.id,
     copper: 0,
     lastFarmed: 0,
-    farmTier: 0
+    farmTier: 0,
+    fatigueLevel: 0
   });
 
   const farmTierIdx = client.farmDb.get(key, 'farmTier');
@@ -25,15 +24,23 @@ exports.run = (client, message, args) => {
     var reply = 'Available farm tiers (average income / upgrade cost):\n';
 
     client.config.farmTiers.forEach(tier => {
-      reply = reply.concat(`  - ${tier.name} (${format.toGSC(tier.averageGain)} / ${format.toGSC(tier.investCost)})\n`)
+      reply = reply.concat(`  - ${tier.name} (${format.toGSC(tier.averageGain, true)} / ${format.toGSC(tier.investCost, true)})\n`)
+      if (reply.length >= 1700) {
+        message.channel.send(reply);
+        reply = '';
+      }
     });
 
-    message.channel.send(reply);
+    if (reply.length > 0) {
+      message.channel.send(reply);
+    }
   }
   else {
     // standard farm + status flow
+    reduceFatigue(client, key);
+
     const currentTime = new Date().getTime();
-    const farmAvailable = client.farmDb.get(key, 'lastFarmed') + FARM_DELAY_MS;
+    const farmAvailable = client.farmDb.get(key, 'lastFarmed') + calculateDelay(client, key);
     const diffMs = farmAvailable - currentTime;
 
     const reply = new Discord.RichEmbed();
@@ -41,15 +48,16 @@ exports.run = (client, message, args) => {
     if (farmAvailable < currentTime) {
       const farmedCopper = getFarmFromAvg(farmTier.averageGain);
       client.farmDb.math(key, "+", farmedCopper, 'copper');
+      client.farmDb.inc(key, 'fatigueLevel');
       client.farmDb.set(key, currentTime, 'lastFarmed');
 
       reply.setColor('#0cf246')
         .setTitle('Farming successful!')
-        .setDescription(`<@${message.author.id}>, you just gained ${format.toGSC(farmedCopper)}! Check back in 20 minutes.`);
+        .setDescription(`<@${message.author.id}>, you just gained ${format.toGSC(farmedCopper)}! Check back in ${format.toDisplayedTime(calculateDelay(client, key))}.`);
     }
     else {
       reply.setColor('#a30a0a')
-        .setTitle('Farming failed!')
+        .setTitle('Farming is not available yet!')
         .setDescription(`<@${message.author.id}>, you can farm in ${format.toDisplayedTime(diffMs)}!`);
     }
 
@@ -57,9 +65,10 @@ exports.run = (client, message, args) => {
       .setThumbnail(message.author.displayAvatarURL)
       .addField('Total gold', format.toGSC(client.farmDb.get(key, 'copper')), true)
       .addField('Farm tier', farmTier.name, true)
-      .addField('Next tier cost', format.toGSC(farmTier.investCost), true)
+      .addField('Next tier cost', format.toGSC(farmTier.investCost, true), true)
+      .addField(`Fatigue level: ${client.farmDb.get(key, 'fatigueLevel')}`, format.toDisplayedTime(calculateDelay(client, key)), true)
       .addBlankField()
-      .setFooter("To upgrade your farm tier, type '!farm invest'");
+      .setFooter("To upgrade your farm, type '!farm invest'. View upgrades with '!farm tiers'.");
     message.channel.send(reply);
   }
 }
@@ -73,6 +82,26 @@ function invest(key, client, message, farmTierIdx, farmTier, totalCopper) {
     client.farmDb.set(key, farmTierIdx + 1, 'farmTier');
     return message.reply(`farm improved! You are now a ${client.config.farmTiers[farmTierIdx+1].name}! GG`)
   }
+}
+
+function reduceFatigue(client, key) {
+  const timeSinceLastFarm = new Date().getTime() - client.farmDb.get(key, 'lastFarmed');
+  const currentDelay = calculateDelay(client, key);
+
+  if (timeSinceLastFarm < currentDelay * 2) {
+    // farmed too soon, not reducing fatigue
+    return;
+  }
+
+  const lvlDrop = Math.floor(timeSinceLastFarm / client.config.fatigue.decreaseIntervalMs);
+  const newLevel = Math.max(0, client.farmDb.get(key, 'fatigueLevel') - lvlDrop);
+  client.farmDb.set(key, newLevel, 'fatigueLevel');
+}
+
+function calculateDelay(client, key) {
+  const fatigueLvl = client.farmDb.get(key, 'fatigueLevel');
+  const cfg = client.config.fatigue;
+  return (cfg.initial + (cfg.scalar * Math.pow(cfg.base, fatigueLvl))) * 60 * 1000;
 }
 
 function getFarmFromAvg(averageGain) {
--- commands/grind.js
+++
@@ -1,5 +0,0 @@
-const Discord = require('discord.js');
-
-exports.run = (client, message, args) => {
-  message.channel.send('!duel accept');
-}
