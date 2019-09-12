8b3d6d46c2388c15424a2adbeaecd6ef17cd04fe
rm-richard <arvarichi@gmail.com>
2019-09-12 13:44:43 +0200

    help text about bog shop

--- commands/help.js
+++ commands/help.js
@@ -4,6 +4,9 @@ Supported commands:
   !farm invest   - upgrade your farm if you have enough gold
   !farm tiers    - list farm upgrades with costs
 
+  !shop          - list items buyable with BOGs
+  !buy 'item'    - buy an item from the BOG shop
+
   !duel @mention - challenge another user to a duel
   !duel accept   - accept a duel challenge
 
@@ -12,8 +15,10 @@ Supported commands:
   !help          - display this message
 
 Concepts:
-  Fatigue: each level adds more delay between farms. Each successful
-           farm increases fatigue by 1. Reduced by 1 every 2 hours.
+      Fatigue: each level adds more delay between farms. Each successful
+               farm increases fatigue by 1. Reduced by 1 every 2 hours.
+          BOG: 'Badge of Gametime' token. You get 1 for every hour spent
+               in WoW. Spend them in the '!shop'.
   Daily bonus: the first farm each day yields extra 400% gold.
 \`\`\``;
 
